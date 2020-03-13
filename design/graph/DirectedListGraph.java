9a78fb64b91cbab9ee03457c460a7a6bdfd5058e
Dmitry Kovalenko <dmrkov@gmail.com>
2020-03-13 14:51:00 +0200

    Added implementation of topological sort

---
+++ design/graph/DirectedListGraph.java
@@ -0,0 +1,62 @@
+package design.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class DirectedListGraph {
+
+    class DirectedEdge {
+        int from;
+        int to;
+
+        public DirectedEdge(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+    }
+
+    List<DirectedEdge>[] edges;
+
+    public DirectedListGraph(int V) {
+        this.edges = new List[V];
+        for (int v = 0; v < V; v++) {
+            this.edges[v] = new ArrayList<>();
+        }
+    }
+
+    public void add(DirectedEdge e) {
+        this.edges[e.from].add(e);
+    }
+
+    public List<DirectedEdge> adj(int v) {
+        return edges[v];
+    }
+
+    public List<Integer> topologicalSort() {
+        List<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[edges.length];
+
+        for (int i = 0; i < edges.length; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, stack);
+            }
+        }
+        while (stack.size() > 0) {
+            result.add(stack.pop());
+        }
+
+        return result;
+    }
+
+    private void dfs(int from, boolean[] visited, Stack<Integer> stack) {
+        visited[from] = true;
+        for (DirectedEdge edge : edges[from]) {
+            if (!visited[edge.to]) {
+                dfs(edge.to, visited, stack);
+            }
+        }
+        stack.add(from);
+    }
+}
